6a0ace8cf7a1ff1acd34656f4c9065c60e1270f2
Mihael Kamenov <Mihael@example.com>
2023-06-05 10:01:52 +0300

    del.

--- My1stPlatform/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$databaseHost = 'localhost';
-$databaseName = 'test5';
-$databaseUsername = 'phpmyadmin';
-$databasePassword = 'Phpmy@dm1n';
-
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
-?>
